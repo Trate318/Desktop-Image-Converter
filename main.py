e4aff20092291479308b530e4298b96c4bb18234
Trate318 <ebeber318@gmail.com>
2025-09-21 23:44:45 -0400

    Basic UI

---
+++ main.py
@@ -0,0 +1,108 @@
+import sys # for copy/paste
+import tkinter as tk
+from tkinterdnd2 import DND_FILES, TkinterDnD
+from PIL import ImageGrab, Image, ImageTk
+from tkinter import filedialog, messagebox
+
+
+VALID_EXTS = (".png", ".jpg", ".jpeg", ".gif", ".bmp")
+
+current_img = None
+
+root = TkinterDnD.Tk()
+root.geometry("600x400")
+root.title("Image Converter")
+root.resizable(False, False)
+root.configure(bg="#c02727")
+
+canvas = tk.Canvas(root, width=600, height=400, bg="#e6ebe9")
+canvas.pack()
+
+input_frame = tk.Frame(root, width=200, height=200, bg="#bdc3c1")
+input_frame.pack_propagate(False)
+
+
+drop_label = tk.Label(
+    input_frame,
+    text="Drag & Drop, Paste, or Click to Enter File",
+    bg=input_frame["bg"],
+    wraplength=180,
+    justify="center",
+    font=("Calibri", 12)
+)
+
+drop_label.pack(expand=True, fill="both")
+
+output_frame = tk.Frame(root, width=200, height=200, bg="#bdc3c1")
+output_frame.pack_propagate(False)
+
+
+filename_input_frame = tk.Frame(
+    root,
+    width=200, 
+    height=40,
+    bg="#A1D8B5"
+    )
+
+
+filename_output_frame = tk.Frame(
+    root,
+    width=200, 
+    height=40,
+    bg="#A1D8B5"
+    )
+
+
+input_buttons_frame = tk.Frame(root, width=200, height=40, bg="#4CB572")
+select_file_type_label = tk.Label(
+    input_buttons_frame, 
+    width=200, 
+    height=40, 
+    bg="#4CB572",
+    text="Select File Type"
+    )
+select_file_type_label.place(x=0, y=0, width=200, height=40)
+
+output_buttons_frame = tk.Frame(root, width=200, height=40, bg="#e6ebe9")
+
+download_label = tk.Label(
+    output_buttons_frame, 
+    width=95, 
+    height=40, 
+    bg="#4CB572",
+    text="Download"
+    )
+download_label.place(x=0, y=0, width=95, height= 40)
+
+copy_label = tk.Label(
+    output_buttons_frame, 
+    width=95, 
+    height=40, 
+    bg="#4CB572",
+    text="Copy"
+    )
+copy_label.place(x=105, y=0, width=95, height= 40)
+
+
+
+convert_frame = tk.Frame(root, width=80, height=40, bg="#4CB572")
+
+convert_label = tk.Label(
+    convert_frame, 
+    width=80, 
+    height=40, 
+    bg="#4CB572",
+    text="Convert"
+    )
+convert_label.place(x=0, y=0, width=80, height= 40)
+
+canvas.create_window(40, 40, anchor="nw", window=input_frame)
+canvas.create_window(40, 300, anchor="nw", window=input_buttons_frame)
+canvas.create_window(300, 200, anchor="center", window=convert_frame)
+canvas.create_window(40, 250, anchor="nw", window=filename_input_frame)
+
+canvas.create_window(600 - 40, 40, anchor="ne", window=output_frame)
+canvas.create_window(600 - 40, 250, anchor="ne", window=filename_output_frame)
+canvas.create_window(600 - 40, 300, anchor="ne", window=output_buttons_frame)
+
+root.mainloop()
